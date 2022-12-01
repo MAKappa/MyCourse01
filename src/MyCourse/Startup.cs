52e70f5ccb032b3766de1e6dde7fa660b1d4f414
Arslan Khohar <khurshid.arslan@gmail.com>
2022-12-01 15:00:44 +0100

    created first controller

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -1,3 +1,6 @@
+using System.Xml.Schema;
+using System.Net.Security;
+using System.Net.Mime;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +19,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +32,23 @@ namespace MyCourse
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
+            //app.UseMvcWithDefaultRoute();
+            
+
+            app.UseMvc(routeBuilder =>
+            {
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+            }); 
         }
     }
 }
