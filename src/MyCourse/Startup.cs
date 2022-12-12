33265f1f4cfb3e45991399ee20a095b8395181ec
Arslan Khohar <khurshid.arslan@gmail.com>
2022-12-12 22:16:34 +0100

    sezione 07

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -19,7 +19,8 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            //services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,17 +38,18 @@ namespace MyCourse
             //app.UseMvcWithDefaultRoute();
             
 
-            app.UseMvc(routeBuilder =>
-            {
-                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
-            });
+            // app.UseMvc(routeBuilder =>
+            // {
+            //     routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            // });
             
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
+                // endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync("Hello World!");
+                // });
             }); 
         }
     }
