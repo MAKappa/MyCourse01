77c6d429220f0c3478a71e77817edf5ec1c2aa6f
Arslan Khohar <khurshid.arslan@gmail.com>
2022-12-14 01:16:27 +0100

    created model,ViewModel and added to a viewCoursesController

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyCourse.Models.Services.Application;
+using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Controllers
 {
@@ -12,7 +14,9 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var courseService = new CourseService();
+            List<CourseViewModel> courses =  courseService.GetServices();
+            return View(courses);
         }
 
         public IActionResult Detail(string id)
