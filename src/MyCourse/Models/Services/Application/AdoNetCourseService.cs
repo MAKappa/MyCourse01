ecb19e8ba63d45f5b05c92d59afba9fe707c8fb2
Arslan Khohar <khurshid.arslan@gmail.com>
2023-06-23 14:41:56 +0200

    Updated to Net6.0

---
+++ src/MyCourse/Models/Services/Application/AdoNetCourseService.cs
@@ -0,0 +1,21 @@
+using MyCourse.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCourse.Models.Services.Application
+{
+    public class AdoNetCourseService : ICourseService
+    {
+        public CourseDetailViewModel GetCourse(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CourseViewModel> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
